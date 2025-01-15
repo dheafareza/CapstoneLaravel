9176d471c7886c5e9e920f245465969d931db176
dheafareza <dheafrz21@gmail.com>
2025-01-15 11:53:08 +0700

    first deploy

---
+++ resources/views/partials/sidebar.blade.php
@@ -0,0 +1,178 @@
+<!-- ======= Sidebar ======= -->
+<aside id="sidebar" class="sidebar">
+
+<ul class="sidebar-nav" id="sidebar-nav">
+
+<div class="d-flex align-items-center justify-content-center p-3 border-bottom">
+    <a href="/" class="logo d-flex flex-column justify-content-center text-decoration-none">
+<!-- <img src="{{ asset('assets/img/logo.png') }}" alt="Logo" style="height: 40px; margin-bottom: 10px;"> -->
+    <span class="d-none d-lg-block text-white justify-content-center" style="font-size: 30px; font-family: 'Poppins', sans-serif;">Sierra Collection</span>
+<!-- <i class="bi bi-list toggle-sidebar-btn text-white mt-2 fs-4" style="cursor: pointer;"></i> -->
+    </a>
+</div>
+
+<!-- Navigation Item -->
+<ul class="nav flex-column mt-3 border-bottom">
+  <li class="nav-item">
+  <a class="nav-link collapsed d-flex align-items-center text-decoration-none text-dark py-2 px-3" href="/">
+      <i class="bi bi-grid"></i>
+      <span class="d-none d-lg-block text-white justify-content-center" style="font-size: 12px; font-family: 'Poppins', sans-serif;">Dashboard</span>
+    </a>
+  </li>
+  </ul>
+  <!-- End Dashboard Nav -->
+    <ul id="components-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+      <li>
+        <a href="components-alerts.html">
+          <i class="bi bi-circle"></i><span>Alerts</span>
+        </a>
+      </li>
+      <li>
+        <a href="components-accordion.html">
+          <i class="bi bi-circle"></i><span>Accordion</span>
+        </a>
+      </li>
+      <li>
+        <a href="components-badges.html">
+          <i class="bi bi-circle"></i><span>Badges</span>
+        </a>
+      </li>
+      <li>
+        <a href="components-breadcrumbs.html">
+          <i class="bi bi-circle"></i><span>Breadcrumbs</span>
+        </a>
+      </li>
+      <li>
+        <a href="components-buttons.html">
+          <i class="bi bi-circle"></i><span>Buttons</span>
+        </a>
+      </li>
+      <li>
+        <a href="components-cards.html">
+          <i class="bi bi-circle"></i><span>Cards</span>
+        </a>
+      </li>
+      <li>
+        <a href="components-carousel.html">
+          <i class="bi bi-circle"></i><span>Carousel</span>
+        </a>
+      </li>
+      <li>
+        <a href="components-list-group.html">
+          <i class="bi bi-circle"></i><span>List group</span>
+        </a>
+      </li>
+      <li>
+        <a href="components-modal.html">
+          <i class="bi bi-circle"></i><span>Modal</span>
+        </a>
+      </li>
+      <li>
+        <a href="components-tabs.html">
+          <i class="bi bi-circle"></i><span>Tabs</span>
+        </a>
+      </li>
+      <li>
+        <a href="components-pagination.html">
+          <i class="bi bi-circle"></i><span>Pagination</span>
+        </a>
+      </li>
+      <li>
+        <a href="components-progress.html">
+          <i class="bi bi-circle"></i><span>Progress</span>
+        </a>
+      </li>
+      <li>
+        <a href="components-spinners.html">
+          <i class="bi bi-circle"></i><span>Spinners</span>
+        </a>
+      </li>
+      <li>
+        <a href="components-tooltips.html">
+          <i class="bi bi-circle"></i><span>Tooltips</span>
+        </a>
+      </li>
+    </ul>
+  <!-- </li> -->
+    <ul id="forms-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+      <li>
+        <a href="forms-elements.html">
+          <i class="bi bi-circle"></i><span>Form Elements</span>
+        </a>
+      </li>
+      <li>
+        <a href="forms-layouts.html">
+          <i class="bi bi-circle"></i><span>Form Layouts</span>
+        </a>
+      </li>
+      <li>
+        <a href="forms-editors.html">
+          <i class="bi bi-circle"></i><span>Form Editors</span>
+        </a>
+      </li>
+      <li>
+        <a href="forms-validation.html">
+          <i class="bi bi-circle"></i><span>Form Validation</span>
+        </a>
+      </li>
+    </ul>
+  </li><!-- End Forms Nav -->
+
+  <li class="nav-heading ">Transaksi</li>
+
+  <li class="nav-item">
+    <a class="nav-link collapsed" href="pendapatans">
+      <i class="bi bi-arrow-up"></i>
+      <span style="font-size: 12px; font-family: 'Poppins', sans-serif;">Pendapatan</span>
+    </a>
+  </li><!-- End Profile Page Nav -->
+
+  <li class="nav-item">
+    <a class="nav-link collapsed" href="pages-faq.html">
+      <i class="bi bi-question-circle"></i>
+      <span>F.A.Q</span>
+    </a>
+  </li><!-- End F.A.Q Page Nav -->
+
+  <li class="nav-item">
+    <a class="nav-link collapsed" href="pages-contact.html">
+      <i class="bi bi-envelope"></i>
+      <span>Contact</span>
+    </a>
+  </li><!-- End Contact Page Nav -->
+
+  <li class="nav-item">
+    <a class="nav-link collapsed" href="pages-register.html">
+      <i class="bi bi-card-list"></i>
+      <span>Register</span>
+    </a>
+  </li><!-- End Register Page Nav -->
+
+  <li class="nav-item">
+    <a class="nav-link collapsed" href="pages-login.html">
+      <i class="bi bi-box-arrow-in-right"></i>
+      <span>Login</span>
+    </a>
+  </li><!-- End Login Page Nav -->
+
+  <li class="nav-item">
+    <a class="nav-link collapsed" href="pages-error-404.html">
+      <i class="bi bi-dash-circle"></i>
+      <span>Error 404</span>
+    </a>
+  </li><!-- End Error 404 Page Nav -->
+
+  <li class="nav-item">
+    <a class="nav-link collapsed" href="pages-blank.html">
+      <i class="bi bi-file-earmark"></i>
+      <span>Blank</span>
+    </a>
+  </li><!-- End Blank Page Nav -->
+
+  <div class="toggle-sidebar-btn">
+<i class="bi bi-chevron-left" id="sidebarToggle"></i>
+</div>
+
+</ul>
+</aside><!-- End Sidebar-->
+
